edc951d2f34dcd54994ec9b109bc0a338dc2f515
shafiankhan <mshafianak@gmail.com>
2024-01-20 23:25:43 -0600

    Website created Successfully

---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}> 
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider> 
+)
